f0ad8afedaf4698fec187701136f69bfb421e5a9
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-10-06 23:43:57 +0300

    Refactor handleDelete in ContactList: add iziToast notification for successful contact deletion

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,11 +2,19 @@ import { useSelector, useDispatch } from "react-redux";
 import style from "./ContactList.module.css";
 import { deleteContact } from "../../redux/contactsOps";
 import { selectContacts } from "../../redux/selectors";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
-  const handleDelete = (id) => dispatch(deleteContact(id));
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+    iziToast.info({
+      title: "Info",
+      message: "Contact deleted successfully",
+    });
+  };
 
   const numberFormat = contacts.map((contact) =>
     contact.number.replace(/(\d{3})(\d{2})(\d{2})/, "$1-$2-$3")
